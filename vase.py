6e6a8628122d5e4b7cfd8a223b846ba1d72b84de
Nahyun Kwon <skgus2624@gmail.com>
2020-10-02 21:34:40 -0500

    code error fixed

--- vase.py
+++ vase.py
@@ -5,19 +5,14 @@ import math
 
 def get_center_point():
     gcode = open("vase_halfscale.gcode", "r")
-
     lines = gcode.readlines()
-
     flag = "no"
-
     first_layer = ""
-
     for l in lines:
         if ";TYPE:WALL-OUTER" in l:
             flag = "yes"
         elif ";TYPE:SKIN" in l:
             flag = "skin"
-
         if flag == "yes":
             first_layer = first_layer + l
         elif flag == "skin":
@@ -27,20 +22,14 @@ def get_center_point():
     # first point
     first_x = float(lines[1].split(" ")[2].split("X")[1])  # x coordinate
     first_y = float(lines[1].split(" ")[3].split("Y")[1])  # y coordinate
-
     max_dist = 0
-
     farthest_point = []
-
     for i in range(2, len(lines)):
         l = lines[i]
         if "X" in l and "Y" in l:
-
             x = float(l.split("X")[1].split(" ")[0])  # x coordinate
             y = float(l.split("Y")[1].split(" ")[0])  # y coordinate
-
             dist = math.sqrt((first_x - x) ** 2 + (first_y - y) ** 2)
-
             if dist > max_dist:
                 max_dist = dist
                 farthest_point.clear()
@@ -48,10 +37,8 @@ def get_center_point():
                 farthest_point.append(y)
         else:
             pass
-
     #print(max_dist)
     #print(farthest_point)
-
     midpoint_x = (first_x + farthest_point[0]) / 2
     midpoint_y = (first_y + farthest_point[1]) / 2
 
@@ -70,30 +57,29 @@ def modifying_gcode():
 
     result = ""
 
+    layers = ""
+
     for l in lines:
-        #result = result+l
+
         if ";TYPE:WALL-OUTER" in l:
             flag = "yes"
         elif ";TYPE:" in l:
             flag = "no"
-
         if flag == "yes" and ";TYPE:WALL-OUTER" not in l:
-            #print(l)
-
             splited = l.split(" ")
-
-            #old_x = ""
-            #old_y = ""
             new_x = ""
             new_y = ""
 
             for i in range(len(splited)):
+
                 if "X" in splited[i]:
                     current_x = float(splited[i].split("X")[1])
                     direction_vector_x = current_x - center_point[0]
                     new_x = str(center_point[0] + (direction_vector_x * random.randint(100, 105) / 100))
                     break
+
             for j in range(len(splited)):
+
                 if "Y" in splited[j]:
                     current_y = float(splited[j].split("Y")[1])
                     direction_vector_y = current_y - center_point[1]
@@ -103,13 +89,14 @@ def modifying_gcode():
             replaced = ""
 
             for k in range(len(splited)):
+
                 if k == i:
                     replaced += "X"+new_x + " "
                 elif k == j:
                     replaced += "Y"+new_y + " "
                 else:
                     replaced += splited[k] + " "
-            print(replaced)
+            #print(replaced)
             l = replaced
             #print(replaced)
 
@@ -118,11 +105,9 @@ def modifying_gcode():
     text_file = open("vase_droop.gcode", "wt")
     text_file.write(result)
     text_file.close()
+
     gcode2 = open("vase_droop.gcode", "r")
-
     droop = gcode2.readlines()
-
-    layers = ""
 
     for ls in droop:
         print(ls)

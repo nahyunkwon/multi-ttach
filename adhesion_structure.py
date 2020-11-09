2639f583a35e2fa24b431b0ba68443eb7d279171
Nahyun Kwon <skgus2624@gmail.com>
2020-11-09 16:42:58 -0600

    try grid structure(failed)

--- adhesion_structure.py
+++ adhesion_structure.py
@@ -150,7 +150,7 @@ def modifying_gcode():
 
 def get_infill_area():
 
-    gcode = open("./cube_adhesion_test.gcode")
+    gcode = open("./CE3_cube_10.gcode")
 
     lines = gcode.readlines()
 
@@ -160,7 +160,7 @@ def get_infill_area():
     target_infill = ""
 
     for l in lines:
-        if ";LAYER:4" in l: # target layer
+        if ";LAYER:25" in l:  # target layer
             is_target = 1
         if is_target == 1 and ";TYPE:FILL" in l:
             is_infill = 1
@@ -174,7 +174,7 @@ def get_infill_area():
     x_values = []
     y_values = []
 
-    print(target_infill)
+    #print(target_infill)
 
     for l in target_infill.split("\n"):
         if "X" in l:
@@ -194,7 +194,7 @@ def get_infill_area():
     y_min = int(y_values[0].split(".")[0]) + 1
     y_max = int(y_values[-1].split(".")[0])
 
-    print(x_max)
+    #print(x_min)
 
     grid_x = []
     grid_y = []
@@ -210,19 +210,53 @@ def get_infill_area():
         grid_y.append(current_y)
         current_y += 2
 
+    # a-structure
 
-    g0 = "G0 F9000 "
-    g1 = "G1 F1200 "
+    g0 = "G0 F9500 "
+    g1 = "G1 F50 "
 
     result = ""
 
     for x in grid_x:
         result += g0 + "X" + str(x) + " Y" + str(grid_y[0]) + "\n"
         result += g1 + "X" + str(x) + " Y" + str(grid_y[-1]) + " E0.5" + "\n"
+    result += "\n"
     for y in grid_y:
         result += g0 + "X" + str(grid_x[0]) + " Y" + str(y) + "\n"
         result += g1 + "X" + str(grid_x[-1]) + " Y" + str(y) + " E0.5" + "\n"
 
+    print(result)
+
+    # b-structure
+
+    result = ""
+
+    current_x = x_min + 1
+    current_y = y_min + 1
+
+    g0 = "G0 F9500 "
+    g1 = "G1 F50 "
+
+    grid_x.clear()
+    grid_y.clear()
+
+    while current_x <= x_max:
+        grid_x.append(current_x)
+        current_x += 2
+
+    while current_y <= y_max:
+        grid_y.append(current_y)
+        current_y += 2
+
+    for x in grid_x:
+        result += g0 + "X" + str(x) + " Y" + str(grid_y[0]) + "\n"
+        result += g1 + "X" + str(x) + " Y" + str(grid_y[0]) + " E3" + "\n"
+    result += "\n"
+    for y in grid_y:
+        result += g0 + "X" + str(grid_x[0]) + " Y" + str(y) + "\n"
+        result += g1 + "X" + str(grid_x[0]) + " Y" + str(y) + " E3" + "\n"
+
+    print("---------------b-------------")
     print(result)
 
 

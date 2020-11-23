0ae70ec9bcfac7c893ab099c87a8eefecab6cd70
Nahyun Kwon <skgus2624@gmail.com>
2020-11-23 14:01:48 -0600

    grid: a-structure

--- adhesion_structure.py
+++ adhesion_structure.py
@@ -7,13 +7,212 @@ from shapely.geometry.polygon import LinearRing, Polygon, Point
 from maxrect import get_intersection, get_maximal_rectangle, rect2poly
 
 
+def get_min_max(input_list):
+    '''
+    get minimum and maximum value in the list
+    :param input_list: list of numbers
+    :return: min, max
+    '''
+
+    min_value = input_list[0]
+    max_value = input_list[0]
+
+    for i in input_list:
+        if i > max_value:
+            max_value = i
+        elif i < min_value:
+            min_value = i
+
+    return min_value, max_value
 
 
-def generate_grid_infill():
+def get_largest_polygon(x_values, y_values):
+    '''
+    Get the largest polygon among multiple polygons (if exist)
+    :param x_values: a list of all x coordinates in infill
+    :param y_values: a list of all y coordinates in infill
+    :return: the list of x-y coordinates of the largest polygon
+    '''
 
-    gcode = open("./bunny.gcode")
+    areas = []
+    polygon_coords = []
+
+    all_polygons = []
+
+    for i in range(len(x_values)):
+        if x_values[i] == "G0":  # next polygon
+            if len(polygon_coords) != 0:
+                areas.append(Polygon(polygon_coords).area)
+                all_polygons.append(polygon_coords)
+                polygon_coords = []
+        else:
+            polygon_coords.append([x_values[i], y_values[i]])
+
+    max_area = areas[0]
+    max_index = 0
+
+    print(areas)
+
+    for i in range(len(areas)):
+        if areas[i] > max_area:
+            max_area = areas[i]
+            max_index = i
+
+    return all_polygons[max_index]
+
+
+def is_far_from_inner_wall(x, y, x_values, y_values, threshold=1):
+    """
+
+    :param x:
+    :param y:
+    :param x_values:
+    :param y_values:
+    :param threshold:
+    :return:
+    """
+
+    for k in range(len(x_values)):
+        if math.hypot(x - x_values[k], y - y_values[k]) < threshold:
+            return False
+
+    return True
+
+
+def get_grid_points_for_target_layer(file, target_layer, gap):
+    '''
+
+    :param file:
+    :param target_layer:
+    :param gap: (mm)
+    :return:
+    '''
+
+    gcode = open(file)
 
     lines = gcode.readlines()
+
+    is_target = 0
+    is_inner_wall = 0
+
+    target_lines = ""
+
+    for l in lines:
+        if is_target == 1 and is_inner_wall == 1 and ";TYPE:WALL-OUTER" in l:
+            break
+        if ";LAYER:" + str(target_layer)+"\n" in l:  # target layer
+            is_target = 1
+            target_lines += l
+        if is_target == 1 and ";TYPE:WALL-INNER" in l:
+            is_inner_wall = 1
+        if is_target == 1 and is_inner_wall == 1:
+            target_lines += l
+
+    x_values = []
+    y_values = []
+
+    for l in target_lines.split("\n"):
+        if "G1" in l:
+            elems = l.split(" ")
+
+            for e in elems:
+                if ";" not in e:
+                    if "X" in e:
+                        x_values.append(float(e.split("X")[1]))
+                    if "Y" in e:
+                        y_values.append(float(e.split("Y")[1]))
+
+        elif "G0" in l:  # flag that indicates the next polygon
+            x_values.append("G0")
+            y_values.append("G0")
+
+    polygon_coords = get_largest_polygon(x_values, y_values)
+
+    polygon_coords.append(polygon_coords[0])
+
+    #print(polygon_coords)
+
+    x_values = []
+    y_values = []
+
+    for i in range(len(polygon_coords)):
+        x_values.append(polygon_coords[i][0])
+        y_values.append(polygon_coords[i][1])
+
+    #print(polygon.area)
+
+    x_min, x_max = get_min_max(x_values)
+    y_min, y_max = get_min_max(y_values)
+
+    grid_x = []
+    grid_y = []
+
+    current_x = x_min + 1
+    current_y = y_min + 1
+
+    #print(x_min)
+    #print(y_min)
+
+    while current_x <= x_max:
+        grid_x.append(current_x)
+        current_x += gap
+    while current_y <= y_max:
+        grid_y.append(current_y)
+        current_y += gap
+
+    #print(grid_x)
+    #print(grid_y)
+
+    # a structure
+    a_x = []
+    a_y = []
+
+    polygon = Polygon(polygon_coords)
+
+    for i in range(len(grid_x)):
+        for j in range(len(grid_y)):
+            current_point = Point(grid_x[i], grid_y[j])
+
+            if polygon.contains(current_point):
+
+                if is_far_from_inner_wall(current_point.x, current_point.y, x_values, y_values):
+                    a_x.append(current_point.x)
+                    a_y.append(current_point.y)
+
+    a_coords = []  # coordinates of a structure
+
+    for i in range(len(a_x)):
+        a_coords.append([a_x[i], a_y[i]])
+
+    print(a_coords)
+
+    print(sorted(a_coords, key=lambda x: x[1]))
+
+    # x and y values for b structure
+    b_x = []
+    b_y = []
+
+    # check if the unit square is included in the polygon
+    for i in range(len(a_coords)):
+        if unit_square_is_included(a_coords[i], gap, a_coords):
+            b_x.append(a_coords[i][0] + gap/2)
+            b_y.append(a_coords[i][1] + gap/2)
+
+    plt.plot(x_values, y_values, 'ro')
+    plt.plot(a_x, a_y, 'bo')
+    plt.plot(b_x, b_y, 'go')
+    plt.show()
+
+    return a_x, a_y, b_x, b_y
+
+
+def generate_grid_infill(a_x, a_y, b_x, b_y, gap, file_name, target_layer):
+
+    gcode = open(file_name)
+
+    lines = gcode.readlines()
+
+    '''
 
     is_target = 0
     is_infill = 0
@@ -72,6 +271,10 @@ def generate_grid_infill():
     while current_y <= y_max:
         grid_y.append(current_y)
         current_y += 2
+        
+    '''
+
+    arbitrary = 0.1
 
     # a-structure
 
@@ -82,24 +285,42 @@ def generate_grid_infill():
 
     layer_height = 0.2
     nozzle_dia = 0.4
-    length = 2 * 0.1
+    length = gap
     fa = ((1.75/2) ** 2) / math.pi
 
-    extrusion = (layer_height * nozzle_dia * length) / fa
+    extrusion = (layer_height * nozzle_dia * length * arbitrary) / fa
 
-    for x in grid_x:
-        result += g0 + "X" + str(x) + " Y" + str(grid_y[0]) + "\n"
-        for i in range(1, len(grid_y)):
-            result += g1 + "X" + str(x) + " Y" + str(grid_y[i]) + " E" + str(extrusion) + "\n"
-    result += "\n"
-    for y in grid_y:
-        result += g0 + "X" + str(grid_x[0]) + " Y" + str(y) + "\n"
-        for i in range(1, len(grid_x)):
-            result += g1 + "X" + str(grid_x[i]) + " Y" + str(y) + " E" + str(extrusion) + "\n"
+    result += g0 + "X" + str(a_x[0]) + " Y" + str(a_y[0]) + "\n"
+
+    for i in range(len(a_x)):
+        if i+1 < len(a_x):
+            if a_x[i+1] == a_x[i]:  # at the same line (y-axis)
+                result += g1 + "X" + str(a_x[i+1]) + " Y" + str(a_y[i+1]) + " E" + str(extrusion)  + "\n"
+            elif a_x[i+1] > a_x[i]:  # next line
+                result += g0 + "X" + str(a_x[i+1]) + " Y" + str(a_y[i+1]) + "\n"
+
+    result += g0 + "X" + str(a_x[0]) + " Y" + str(a_y[0]) + "\n"
+
+    a_coords = []  # coordinates of a structure
+
+    for i in range(len(a_x)):
+        a_coords.append([a_x[i], a_y[i]])
+
+    y_sorted = sorted(a_coords, key=lambda k: k[1])
+
+    result += g0 + "X" + str(a_x[0]) + " Y" + str(a_y[0]) + "\n"
+
+    for i in range(len(y_sorted)):
+        if i+1 < len(y_sorted):
+            if y_sorted[i+1][1] == y_sorted[i][1]:  # at the same line (x-axis)
+                result += g1 + "X" + str(y_sorted[i+1][0]) + " Y" + str(y_sorted[i+1][1]) + " E" + str(extrusion) + "\n"
+            elif a_x[i + 1] > a_x[i]:  # next line
+                result += g0 + "X" + str(y_sorted[i+1][0]) + " Y" + str(y_sorted[i+1][1]) + "\n"
 
     print(result)
 
     # b-structure
+
 
     result = ""
 
@@ -139,190 +360,6 @@ def generate_grid_infill():
     print(result)
 
 
-def get_min_max(input_list):
-    '''
-    get minimum and maximum value in the list
-    :param input_list: list of numbers
-    :return: min, max
-    '''
-
-    min_value = input_list[0]
-    max_value = input_list[0]
-
-    for i in input_list:
-        if i > max_value:
-            max_value = i
-        elif i < min_value:
-            min_value = i
-
-    return min_value, max_value
-
-
-def get_largest_polygon(x_values, y_values):
-    '''
-    Get the largest polygon among multiple polygons (if exist)
-    :param x_values: a list of all x coordinates in infill
-    :param y_values: a list of all y coordinates in infill
-    :return: the list of x-y coordinates of the largest polygon
-    '''
-
-    areas = []
-    polygon_coords = []
-
-    all_polygons = []
-
-    for i in range(len(x_values)):
-        if x_values[i] == "G0":  # next polygon
-            if len(polygon_coords) != 0:
-                areas.append(Polygon(polygon_coords).area)
-                all_polygons.append(polygon_coords)
-                polygon_coords = []
-        else:
-            polygon_coords.append([x_values[i], y_values[i]])
-
-    max_area = areas[0]
-    max_index = 0
-
-    print(areas)
-
-    for i in range(len(areas)):
-        if areas[i] > max_area:
-            max_area = areas[i]
-            max_index = i
-
-    return all_polygons[max_index]
-
-
-def is_far_from_inner_wall(current_x, current_y, x_values, y_values):
-
-    for k in range(len(x_values)):
-        print(math.hypot(current_x - x_values[k], current_y - y_values[k]))
-        if math.hypot(current_x - x_values[k], current_y - y_values[k]) < 1:
-            return False
-
-    return True
-
-
-def get_target_points(file, target_layer, gap):
-    '''
-    get the target rectangles that fit inner-wall
-    :return: list of grid coordinates
-    '''
-
-    gcode = open(file)
-
-    lines = gcode.readlines()
-
-    is_target = 0
-    is_inner_wall = 0
-
-    target_lines = ""
-
-    for l in lines:
-        if is_target == 1 and is_inner_wall == 1 and ";TYPE:WALL-OUTER" in l:
-            break
-        if ";LAYER:" + str(target_layer)+"\n" in l:  # target layer
-            is_target = 1
-            target_lines += l
-        if is_target == 1 and ";TYPE:WALL-INNER" in l:
-            is_inner_wall = 1
-        if is_target == 1 and is_inner_wall == 1:
-            target_lines += l
-
-    x_values = []
-    y_values = []
-
-    for l in target_lines.split("\n"):
-        if "G1" in l:
-            elems = l.split(" ")
-
-            for e in elems:
-                if ";" not in e:
-                    if "X" in e:
-                        x_values.append(float(e.split("X")[1]))
-                    if "Y" in e:
-                        y_values.append(float(e.split("Y")[1]))
-
-        elif "G0" in l:  # flag that indicates the next polygon
-            x_values.append("G0")
-            y_values.append("G0")
-
-    polygon_coords = get_largest_polygon(x_values, y_values)
-
-    polygon_coords.append(polygon_coords[0])
-
-    print(polygon_coords)
-
-    x_values = []
-    y_values = []
-
-    for i in range(len(polygon_coords)):
-        x_values.append(polygon_coords[i][0])
-        y_values.append(polygon_coords[i][1])
-
-    #print(polygon.area)
-
-    x_min, x_max = get_min_max(x_values)
-    y_min, y_max = get_min_max(y_values)
-
-    grid_x = []
-    grid_y = []
-
-    current_x = x_min + 1
-    current_y = y_min + 1
-
-    print(x_min)
-    print(y_min)
-
-    while current_x <= x_max:
-        grid_x.append(current_x)
-        current_x += gap
-    while current_y <= y_max:
-        grid_y.append(current_y)
-        current_y += gap
-
-    print(grid_x)
-    print(grid_y)
-
-    # a structure
-    a_x = []
-    a_y = []
-
-    polygon = Polygon(polygon_coords)
-
-    for i in range(len(grid_x)):
-        for j in range(len(grid_y)):
-            current_point = Point(grid_x[i], grid_y[j])
-
-            if polygon.contains(current_point):
-
-                if is_far_from_inner_wall(current_point.x, current_point.y, x_values, y_values):
-                    a_x.append(current_point.x)
-                    a_y.append(current_point.y)
-
-    a_coords = []  # coordinates of a structure
-
-    for i in range(len(a_x)):
-        a_coords.append([a_x[i], a_y[i]])
-
-    # x and y values for b structure
-    b_x = []
-    b_y = []
-
-    # check if the unit square is included in the polygon
-    for i in range(len(a_coords)):
-        if unit_square_is_included(a_coords[i], gap, a_coords):
-            b_x.append(a_coords[i][0] + gap/2)
-            b_y.append(a_coords[i][1] + gap/2)
-
-    plt.plot(x_values, y_values, 'ro')
-    plt.plot(a_x, a_y, 'bo')
-    plt.plot(b_x, b_y, 'go')
-    plt.show()
-
-    return a_x, a_y
-
-
 def unit_square_is_included(p, gap, coords):
     if [p[0] + gap, p[1]] not in coords:
         return False
@@ -335,6 +372,6 @@ def unit_square_is_included(p, gap, coords):
 
 
 if __name__ == "__main__":
-    #get_target_points("./CE3_cube.gcode", 10, 2)
-    #get_target_points("./cylinder.gcode", 20, 2)
-    get_target_points("./bunny.gcode", 13, 1.5)
+    get_grid_points_for_target_layer("./CE3_cube.gcode", 10, 2)
+    #get_grid_points_for_target_layer("./cylinder.gcode", 20, 2)
+    #get_grid_points_for_target_layer("./bunny.gcode", 13, 2)
